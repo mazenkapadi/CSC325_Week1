b36cb8c3173937acc75e14567be1181f4e73b469
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-09-01 12:16:02 -0400

    test commit

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -1,2 +1,47 @@
-package org.example;public class Course {
+package org.example;
+
+public class Course {
+    protected int ID;
+    protected String Name;
+    protected String Code;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String Code) {
+        this.Code = Code;
+    }
+
+    Course()    {
+        this.ID = 0;
+        this.Name = "Software Engineering";
+        this.Code = "CSC 325";
+    }
+
+    Course(int _id, String _name, String _code)    {
+        this.ID = _id;
+        this.Name = _name;
+        this.Code = _code;
+    }
+
+    public String toString()    {
+        return "Course ID: " + this.ID + "\nCourse Name: " + this.Name + "\nCourse Code: " + this.Code;
+    }
 }
--- src/main/java/org/example/NewClass.java
+++ src/main/java/org/example/NewClass.java
@@ -1,19 +1,30 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class NewClass {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Instantiating 2 objects to test default and parametric constructors
+        Course course1 = new Course();
+        Course course2 = new Course(1, "Software Engineering", "CSC 325");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        //Intantiating an objects to test setters and getters explicitly
+        Course course3 = new Course();
+
+        //testing setter methods
+        course3.setID(2);
+        course3.setName("Software Engineering II");
+        course3.setCode("CSC 326");
+
+        //testing default constructor
+        System.out.println(course1.toString());
+
+        //testing parametric constructor
+        System.out.println(course2.toString());
+
+        //testing setters and getters
+        System.out.println(course3.toString());
+
+        //testing getName method
+        System.out.println("Expected: Software Engineering II \nActual: " + course3.getName());
     }
 }
